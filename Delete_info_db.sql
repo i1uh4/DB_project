69a9d6d517bf008d193eff0a3d00f3f023297ffb
i1uh4 <105651496+i1uh4@users.noreply.github.com>
2023-12-02 09:41:31 +0300

    Добавление файла с функциями для очистки БД

---
+++ Delete_info_db.sql
@@ -0,0 +1,43 @@
+------ Очистка таблиц ------
+
+-- Удаление всех таблиц
+DROP FUNCTION IF EXISTS clear_all_tables();
+CREATE FUNCTION clear_all_tables()
+	RETURNS void AS
+	$$
+	DELETE FROM product;
+	DELETE FROM user_info;
+	DELETE FROM user_statistic;
+    DELETE FROM favourites;
+    DELETE FROM account;
+    DELETE FROM meal;
+	$$
+
+LANGUAGE sql;
+
+-- Удаление таблицы account
+
+DROP FUNCTION IF EXISTS clear_account_table(INTEGER);
+CREATE FUNCTION clear_account_table(curr_account_id INTEGER)
+	RETURNS void AS
+	$$
+	DELETE FROM account
+    WHERE id = curr_account_id;
+	$$
+
+LANGUAGE sql;
+
+-- Удаление продукта из категории избранное --
+DROP FUNCTION IF EXISTS delete_fav_product(INTEGER, INTEGER);
+CREATE FUNCTION delete_fav_product(curr_user_id INTEGER, curr_product_id INTEGER)
+	RETURNS void AS
+	$$
+	DELETE FROM favourites
+    WHERE user_id = curr_user_id AND product_id = curr_product_id;
+	$$
+
+LANGUAGE sql;
+
+
+
+
